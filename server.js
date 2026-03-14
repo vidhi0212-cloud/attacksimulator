570b90b7a27c4bab60829ae648d38658e015ee2a
vidhi0212-cloud <ranavidhi1234567@gmail.com>
2026-03-15 03:37:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const DATA_DIR = path.join(__dirname, 'data');
 const DB_PATH = path.join(DATA_DIR, 'db.json');
 
